82bacb545d01420bb72a7f97c0c94fd4ca43c6c8
aminwhat <m.aminwhat@gmail.com>
2022-08-27 23:03:26 +0430

    prisma create user problem

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as morgan from 'morgan';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(morgan('tiny'));
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // this will pass the arguments that are not in the dto
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
